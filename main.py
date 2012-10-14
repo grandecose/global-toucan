98c9084760af1f8ff18f27b8786abdc7de763cb9
grandecose <dscinic@gmail.com>
2012-10-14 19:52:45 -0400

    Cleaned up stuff in the python file. Updated license file. Added time.

--- main.py
+++ main.py
@@ -1,60 +1,66 @@
 on = 1;
 a = -9.800000000000
-print("Welcome to the physics calculator\n");
+print("Welcome to Global Toucan, a physics calculator.\n");
 valid = "Please choose a valid option\n";
 while(on == 1):
 	a = -9.800000000000000000000000
-	choice = int(input("What equation would like to find?\n1. Final Velocity\n2. Distance\n3. Time\n4. Help\n5. Quit\n"))
-	if(choice == 1):
-		z = int(input("What do you want to find?\n1. Final Velocity\n2. Initial Velocity\n3. Acceleration\n4. Time\n"));
-		if(z == 1):
-			vi = int(input("What is the initial velocity?\n"))
+	choice = raw_input("What equation would like to find?\n1. Final Velocity\n2. Distance\n3. Time\n4. Help\n5. List Equations\n6. Quit\n")
+	if(choice == "1"):
+		z = raw_input("What do you want to find?\n1. Final Velocity\n2. Initial Velocity\n3. Acceleration\n4. Time\n");
+		if(z == "1"):
+			vi = int(input("What is the initial velocity?\n"));
 			
-			time = int(input("What is the time?\n"))
+			time = int(input("What is the time?\n"));
 
-			vf = vi + (a * time)
+			vf = (vi * 1.000000000000000000000000000000000) + (a * time)
 
-			print("Your final velocity is: ")
+			print("Your final velocity is %s m/s" % vf);
 
-			print vf
-
-		elif(z == 2):
-			vf = int(input("What is the final velocity (m/s)?"))
+		elif(z == "2"):
+			vf = int(input("What is the final velocity?\n"));
 			
-			time = int(input("For how long is the object travelling (seconds)?"))
+			time = int(input("For how long is the object travelling?\n"));
 
-			vi = vf - (a * time)
+			vi = (vf * 1.00000000000000000000000000) - (a * time)
 			
-			print("The initial velocity is: ")
-
-			print vi			
-		elif(z == 3):
-			vf = int(input("What is the final velocity?\n"))
+			print("The initial velocity is %s m/s" % vi);
+		
+		elif(z == "3"):
+			vf = int(input("What is the final velocity?\n"));
 			
-			vi = int(input("What is your initial velocity?\n"))
+			vi = int(input("What is your initial velocity?\n"));
 
-			time = int(input("What is the time?\n"))	
+			time = int(input("What is the time?\n"));
 			
 			a = 1.000000000000000 * ((vf - vi) / time)
 
-			print("The acceleration is: ");
+			print("The acceleration is %s m/s/s" % a);
+		elif(z == "4"):
+			vf = int(input("What is the final velocity?\n"));
 			
-			print a
-		elif(z == 4):
-			print valid
+			vi = int(input("What is the initial velocity?\n"));
+
+			time = (vf - vi) / (a * 1.0000000000000000000000000)
+ 		
+			print("The time is %s seconds" % time);	
 		else:
 			print valid
 
-	elif(choice == 2):
+	elif(choice == "2"):
 		print valid
-	elif(choice == 3):
+	elif(choice == "3"):
 		print valid
 
-	elif(choice == 4):
-		print("Physics calculator is a simple program written in python in order to physics problems.\n The calculator will only take numbers, so please no units.\n Everything will be done in seconds for time, m/s for velocity, and m/s/s for acceleration.\n");
-		raw_input("Press enter to return to the main page.\n");
-	elif(choice == 5):
-		jdfhks = raw_input("Physics Calculator will now exit, please press enter.")
+	elif(choice == "4"):
+		print("Global Toucan is a simple program written in python made to solve physics problems concerning motion.\n The calculator will only take numbers, so please no units.\nWhen calculating for directions going down please remember to include '-'. For example if your calculation involves an apple falling 50 meters one would input -50 as the distance.\nEverything will be done in seconds for time, m/s for velocity, and m/s/s for acceleration.\nAs such, please do any conversions beforehand.\nGlobal Toucan is licensed under the GPLv3 a Free Software License.\n");
+		raw_input("Press enter to return to the main menu.\n");
+	
+	elif(choice == "5"):
+		print("The following are the equations used by this program:\nFinal Velocity: vf = vi + at, where vf is final velocity, a is acceleration and t is time.\nDistance: d = vi * t + .5 a * (t ^ 2), where d is distance, vi is initial velocity, t is time, a is acceleration, and t is time.\n");
+		raw_input("Press enter when you are ready to return to the main menu.\n");
+		
+	elif(choice == "6"):
+		jdfhks = raw_input("Global Toucan will now exit, please press enter.")
 		on = 0;
 
 	else:
